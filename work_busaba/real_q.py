Fix units

work_busaba/real_q.py
@@ -29,10 +29,10 @@ qctrl = Qctrl()
 # In[2]:
 
 
-max_drive_amplitude = 2 * np.pi * 20            # MHz
-control_count = 1                               # 5
-segment_count = 64                              # 16
-duration = 5 * np.pi / (max_drive_amplitude)    # 30.0
+max_drive_amplitude = 2 * np.pi * 20                  # MHz
+control_count = 1
+segment_count = 64
+duration = 5 * np.pi / (max_drive_amplitude) * 1000   # Convert to ns
 shot_count = 32
 
 