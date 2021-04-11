c69571d0b17f51e0a96e5ba2820c1478adac9990
Mike Taylor <mtayl29@u.rochester.edu>
2021-04-11 01:51:55 -0400

    Clean up Code

--- work_mike/sim_not.py
+++ work_mike/sim_not.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-#! /usr/bin/python
-#
-# Author: Gaute Hope (gaute.hope@nersc.no) / 2015
-#
-# based on example from matlab sinc function and
-# interpolate.m by H. Hobæk (1994).
-#
-# this implementation is similar to the matlab sinc-example, but
-# calculates the values sequentially and not as a single matrix
-# matrix operation for all the values.
-#
-
+import sys
 import scipy as sc
 import numpy as np
 
@@ -75,31 +64,6 @@ def upsample (x, k):
 
   return interp (xp, xt, x)
 
-def upsample3 (x, k, workers = None):
-  """
-  Like upsample, but uses the multi-threaded interp3
-  """
-
-  assert k >= 1, 'k must be equal or greater than 1'
-
-  mn = x.shape
-  if len(mn) == 2:
-    m = mn[0]
-    n = mn[1]
-  elif len(mn) == 1:
-    m = 1
-    n = mn[0]
-  else:
-    raise ValueError ("x is greater than 2D")
-
-  nn = n * k
-
-  xt = np.linspace (1, n, n)
-  xp = np.linspace (1, n, nn)
-
-  return interp3 (xp, xt, x, workers)
-
-
 def interp (xp, xt, x):
   """
   Interpolate the signal to the new points using a sinc kernel
@@ -133,107 +97,10 @@ def interp (xp, xt, x):
 
   return y.squeeze ()
 
-default_workers = 6
-def interp3 (xp, xt, x, workers = default_workers):
-  """
-  Interpolate the signal to the new points using a sinc kernel
-
-  Like interp, but splits the signal into domains and calculates them
-  separately using multiple threads.
-
-  input:
-  xt    time points x is defined on
-  x     input signal column vector or matrix, with a signal in each row
-  xp    points to evaluate the new signal on
-  workers  number of threaded workers to use (default: 16)
-
-  output:
-  y     the interpolated signal at points xp
-  """
-
-  mn = x.shape
-  if len(mn) == 2:
-    m = mn[0]
-    n = mn[1]
-  elif len(mn) == 1:
-    m = 1
-    n = mn[0]
-  else:
-    raise ValueError ("x is greater than 2D")
-
-  nn = len(xp)
-
-  y = np.zeros((m, nn))
-
-  # from upsample
-  if workers is None: workers = default_workers
-
-  xxp = np.array_split (xp, workers)
-
-  from concurrent.futures import ThreadPoolExecutor
-  import concurrent.futures
-
-  def approx (_xp, strt):
-    for (pi, p) in enumerate (_xp):
-      si = np.tile (np.sinc (xt - p), (m, 1))
-      y[:, strt + pi] = np.sum (si * x)
-
-  jobs = []
-  with ThreadPoolExecutor (max_workers = workers) as executor:
-    strt = 0
-    for w in np.arange (0, workers):
-      f = executor.submit (approx, xxp[w], strt)
-      strt = strt + len (xxp[w])
-      jobs.append (f)
-
-
-  concurrent.futures.wait (jobs)
-
-  return y.squeeze ()
-
-def upsample2 (x, k):
-  """
-  Upsample the signal to the new points using a sinc kernel. The
-  interpolation is done using a matrix multiplication.
-
-  Requires a lot of memory, but is fast.
-
-  input:
-  xt    time points x is defined on
-  x     input signal column vector or matrix, with a signal in each row
-  xp    points to evaluate the new signal on
-
-  output:
-  y     the interpolated signal at points xp
-  """
-
-  mn = x.shape
-
-  if len(mn) == 2:
-    m = mn[0]
-    n = mn[1]
-  elif len(mn) == 1:
-    m = 1
-    n = mn[0]
-  else:
-    raise ValueError ("x is greater than 2D")
-
-  nn = n * k
-
-  [T, Ts]  = np.mgrid[1:n:nn*1j, 1:n:n*1j]
-  TT = Ts - T
-  del T, Ts
-
-  y = np.sinc(TT).dot (x.reshape(n, 1))
-
-  return y.squeeze()
-
 # In[1]:
-
-import matplotlib.pyplot as plt
-
 from qctrlvisualizer import get_qctrl_style, plot_controls
 from qctrl import Qctrl
+
 
 qctrl = Qctrl()
 
@@ -465,14 +332,6 @@ optimization_result = qctrl.functions.calculate_optimization(
 
 print("Infidelity of gate: " + str(optimization_result.cost))
 
-fig = plt.figure()
-plot_controls(
-    fig,
-    controls={
-        "$\\Omega$": optimization_result.output["Omega"],
-    }, polar=False)
-plt.show()
-
 # In[7]:
 
 # Test optimized pulse on more realistic qubit simulation
@@ -497,8 +356,9 @@ print(ideal_not_gate)
 print("NOT Gate Error: " + str(not_error))
 
 # In[81]
-# smoothed_real = upsample(optimized_values.real,2)
-# smoothed_imag = upsample(optimized_values.imag,2)
+
+# Perform a Sinc Interpolation on the amplitudes of the pulse
+
 smoothed_amp = upsample(np.absolute(optimized_values),2)
 
 smoothed_phase = []
@@ -510,7 +370,6 @@ max_amp = max(smoothed_amp)
 
 smoothed_amp = smoothed_amp / max_amp
 
-# smoothed = smoothed_real + 1j * smoothed_imag
 smoothed = smoothed_amp * np.exp(1j*np.angle(smoothed_phase))
 
 
@@ -521,9 +380,17 @@ with open("sphase.txt", "w") as sphase_f:
     for val in smoothed_phase:
         sphase_f.write("{}\n".format(np.angle(val)))
 
+smoothed_amp_phase = np.stack((smoothed_amp,smoothed_phase),axis=1)
+
+print(smoothed_amp_phase.shape)
+
+np.save("NOT_START_S.npy",smoothed_amp_phase)
+
+
+# Test interpolated pulse against the more realistic simulation
+
 result = simulate_more_realistic_qubit(duration=duration, values=smoothed, shots=1024, repetitions=1)
 
-# In[82]:
 realized_not_gate = result["unitary"]
 not_error = error_norm(realized_not_gate, ideal_not_gate)
 
@@ -544,6 +411,8 @@ for val in optimized_values:
     absolutes += [np.absolute(val)]
 max_amp = max(absolutes)
 
+
+
 # Write parameters to file
 with open("amplitude.txt", "w") as amplitude_f:
     for val in absolutes:
@@ -552,6 +421,10 @@ with open("phase.txt", "w") as phase_f:
     for val in optimized_values:
         phase_f.write("{}\n".format(np.angle(val)))
 
+unsmoothed_amp_phase = np.stack((absolutes,np.angle(optimized_values)),axis=1)
 
+print(unsmoothed_amp_phase.shape)
+
+np.save("NOT_START_U.npy",unsmoothed_amp_phase)
 
 
